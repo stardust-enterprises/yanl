30a060e5f480be8ddd8beb80299abd1cfc510810
xTrM-EN <26600206+xtrm-en@users.noreply.github.com>
2021-11-21 02:42:46 +0100

    test
    Signed-off-by: xTrM-EN <26600206+xtrm-en@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ val api = kotlin.sourceSets.create("api") {
     kotlin.srcDir("src/api/kotlin")
     resources.srcDir("src/api/resources")
 }
-
 kotlin.sourceSets["main"].dependsOn(api)
 
 group = "fr.stardustenterprises"
