ceb912660859d30455812183d6b002be40c8c0aa
lambdagg <lambda@jikt.im>
2022-01-09 13:10:25 +0100

    api: updated exceptions.kt

--- src/api/kotlin/fr/stardustenterprises/yanl/api/exceptions.kt
+++ src/api/kotlin/fr/stardustenterprises/yanl/api/exceptions.kt
@@ -9,12 +9,12 @@ class NativeNotFoundException(
     /**
      * The path to the target native.
      */
-    native: String,
+    libraryName: String,
 
     /**
      * The message to show as an exception.
      */
-    message: String = "Native \"$native\" wasn't found!",
+    message: String = "Library \"$libraryName\" wasn't found!",
 
     /**
      * Another throwable defining the cause of that Exception. Generally null.
@@ -32,10 +32,10 @@ class UnsupportedPlatformException(
     /**
      * The message to show as an exception.
      */
-    extra: String = "No extra information provided.",
+    information: String = "no extra information provided",
 
     /**
      * Another throwable defining the cause of that Exception. Generally null.
      */
     cause: Throwable? = null
-) : Exception("Unsupported platform. ($extra)", cause)
+) : Exception("Unsupported platform ($information).", cause)
